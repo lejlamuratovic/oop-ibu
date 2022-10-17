69513b3dff3b05fef4dcf4ed73756f182bcbe681
Lejla Muratović <114357846+lejlamuratovic@users.noreply.github.com>
2022-10-17 22:04:53 +0200

    Add files via upload

---
+++ exercise2.java
@@ -0,0 +1,6 @@
+public class exercise2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("(And all the people of the world)");
+    }  
+}
