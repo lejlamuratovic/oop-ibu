318d653660cb4b0aa9124c08ff144af94e2933b5
Lejla Muratović <114357846+lejlamuratovic@users.noreply.github.com>
2022-10-17 22:08:36 +0200

    Add files via upload

---
+++ exercise3.java
@@ -0,0 +1,10 @@
+public class exercise3 {
+    public static void main(String[] args) {
+        //Create a program that counts how many seconds there are in a year. 
+        //You can assume that a year consists of 365 days (therefore the year is not a leap year). 
+
+        int numberOfSeconds = 60 * 60 * 24 * 365;
+
+        System.out.println("There are " + numberOfSeconds + " seconds in a year");
+    }
+}
