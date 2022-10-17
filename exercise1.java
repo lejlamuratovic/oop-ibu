d74db6d184be733b17ec8967716b979c965949dc
Lejla Muratović <114357846+lejlamuratovic@users.noreply.github.com>
2022-10-17 22:01:17 +0200

    Add files via upload

---
+++ exercise1.java
@@ -0,0 +1,6 @@
+public class exercise1 {
+    public static void main(String[] args) {
+        //Create a program that prints your name to the screen.
+         System.out.println("Lejla Muratovic");
+    }  
+  }
