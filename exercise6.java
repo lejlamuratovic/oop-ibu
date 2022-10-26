7514d435f5ffb5545db290df46b820148335a559
Lejla Muratović <114357846+lejlamuratovic@users.noreply.github.com>
2022-10-26 20:42:36 +0200

    Update exercise6.java

--- exercise6.java
+++ exercise6.java
@@ -1,10 +1,22 @@
+import java.util.Scanner; 
+
 public class exercise6 {
     public static void main(String[] args) {
         //Create a method printText that prints the following string of characters: "In the beginning there were the swamp, the hoe and Java." and a line break.
-        printText();
+        //Develop the program by adding the following feature: the main program should ask the user how many times the text should be printed
+        //(meaning how many times the method is called).
+        Scanner reader = new Scanner(System.in); 
+
+        System.out.println("How many times should the text be printed: ");
+
+        int amount = reader.nextInt();
+        printText(amount);
     }
 
-public static void printText() {
-    System.out.println("In the beginning there were the swamp, the hoe and Java.");
+public static void printText(int amount) {
+    for(int i = 0; i < amount; i++) {
+        System.out.println("In the beginning there were the swamp, the hoe and Java.");
+    }
 }
 }
+
